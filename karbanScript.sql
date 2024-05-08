4e2feb832dac6021f020a22c3c2ba23d29e95f4b
realFK98 <wishira2013.coth@gmail.com>
2024-05-08 16:16:49 +0700

    fix script

--- karbanScript.sql
+++ karbanScript.sql
@@ -81,4 +81,32 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-04-27 16:45:55
-SELECT * FROM karban.status;
+
+CREATE TABLE IF NOT EXISTS `karban`.`tasksV2` (
+                                                  `id_task` INT NOT NULL AUTO_INCREMENT,
+                                                  `title` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(500) NULL DEFAULT NULL,
+    `created_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `task_status` VARCHAR(50) NOT NULL DEFAULT 'No Status',
+    `assignees` VARCHAR(30) NULL DEFAULT NULL,
+    PRIMARY KEY (`id_task`),
+    INDEX `fk_Tasks_status_idx` (`task_status` ASC) VISIBLE,
+    CONSTRAINT `fk_Tasks_statusV2`
+    FOREIGN KEY (`task_status`)
+    REFERENCES `karban`.`statusV2` (`status_name`))
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+
+LOCK TABLES `tasksV2` WRITE;
+INSERT INTO tasksV2(title,description,created_on,updated_on,task_status,assignees) VALUES
+                                                                                       ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0',
+                                                                                        'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5','2024-04-22 09:00:00','2024-04-22 09:00:00','No Status','Assignees1Assignees2Assignees3'),
+                                                                                       ('Repository',null,'2024-04-22 09:05:00','2024-04-22 14:00:00','To Do',null),
+                                                                                       ('ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','2024-04-22 09:10:00','2024-04-25 00:00:00','Doing','あなた、彼、彼女 (私ではありません)'),
+                                                                                       ('_Infrastructure_','_Setup containers_','2024-04-22 09:15:00','2024-04-22 10:00:00','Done','ไก่งวง กับ เพนกวิน');
+UNLOCK TABLES;
