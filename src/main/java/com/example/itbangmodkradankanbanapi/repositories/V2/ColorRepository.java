5d59e18d21acaf20db7300cd777f42e963604d6a
realFK98 <wishira2013.coth@gmail.com>
2024-05-15 17:27:27 +0700

    Merge branch 'V2'

--- src/main/java/com/example/itbangmodkradankanbanapi/repositories/V2/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.itbangmodkradankanbanapi.repositories.V2;
-
-import com.example.itbangmodkradankanbanapi.entities.V2.ColorStatus;
-import com.example.itbangmodkradankanbanapi.entities.V2.StatusV2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColorRepository extends JpaRepository<ColorStatus,Integer> {
-
-}
