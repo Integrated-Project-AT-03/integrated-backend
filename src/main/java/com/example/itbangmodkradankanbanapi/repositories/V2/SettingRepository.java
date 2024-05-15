cdf501c6e2a78e3d18f8c17a98930084fde44378
realFK98 <wishira2013.coth@gmail.com>
2024-05-15 17:22:13 +0700

    Revert "Merge branch 'V2'"
    This reverts commit e0746fdb05ef9a7cfa3581f13ae32ee377cfa8e4, reversing
    changes made to 4bc3fa6e5dee2790e3bcf4005d588a74268fbdc8.

--- src/main/java/com/example/itbangmodkradankanbanapi/repositories/V2/SettingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.itbangmodkradankanbanapi.repositories.V2;
-
-import com.example.itbangmodkradankanbanapi.entities.V2.ColorStatus;
-import com.example.itbangmodkradankanbanapi.entities.V2.Setting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SettingRepository extends JpaRepository<Setting,Integer> {
-
-}
